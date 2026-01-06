46e4ca65ba6fd3d2cd7fe16cecbf2b029415e3ad
tarik.atasoy <tarik.atasoy@detaysoft.com>
2026-01-06 23:46:28 +0300

    api düzenleme

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 
 import { Order, OrderType, Position, PositionSide, Wallet } from "../types";
 
-const API_BASE_URL = 'http://localhost:3002/api';
+const API_BASE_URL = 'https://tarikatasoy/api';
 
 class ApiService {
   private token: string | null = null;
